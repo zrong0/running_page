817d45e88646f28c8ac1c9c96025800c5aacf2ec
zrong0 <zoerong916@gmail.com>
2024-11-07 16:37:16 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -14,16 +14,7 @@ const data: ISiteMetadataResult = {
   siteUrl: 'https://run.zxr.me',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
-  // navLinks: [
-  //   {
-  //     name: 'Blog',
-  //     url: 'https://github.com/yihong0618/gitblog',
-  //   },
-  //   {
-  //     name: 'About',
-  //     url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-  //   },
-  // ],
+  navLinks: [],
 };
 
 export default data;
