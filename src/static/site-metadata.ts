38a8014cabba917c0071783f9f0c4ca4cab0ad92
zrong0 <zoerong916@gmail.com>
2024-11-07 16:35:46 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,19 +11,19 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://run.zxr.me',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
-  navLinks: [
-    {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
-    },
-  ],
+  // navLinks: [
+  //   {
+  //     name: 'Blog',
+  //     url: 'https://github.com/yihong0618/gitblog',
+  //   },
+  //   {
+  //     name: 'About',
+  //     url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+  //   },
+  // ],
 };
 
 export default data;
